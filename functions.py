529c9c07803a4526aad0761b92f5fa1ce20b3e15
Eduardo <eduardo.queiroz@investimentos.one>
2024-04-08 12:41:22 -0300

    atualiza docstring

--- functions.py
+++ functions.py
@@ -1,3 +1,16 @@
+"""_summary_
+"""
+
+
 def funcao_soma(a: int, b: int) -> int:
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        int: _description_
+    """
 
     return a + b
