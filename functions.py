d4c1100c0a9cc081e3f11f36e39087249a11f734
Eduardo <eduardo.queiroz@investimentos.one>
2024-04-08 12:45:57 -0300

    jdfaçksdfjadkls

--- functions.py
+++ functions.py
@@ -1,16 +1,3 @@
-"""_summary_
-"""
-
-
 def funcao_soma(a: int, b: int) -> int:
-    """_summary_
-
-    Args:
-        a (int): _description_
-        b (int): _description_
-
-    Returns:
-        int: _description_
-    """
 
     return a + b
